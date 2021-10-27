1efc6cb807987c528957282965a8b8c743913bf8
Richard <714292024@qq.com>
2021-10-27 10:10:50 +0800

    main-初始化提交

---
+++ src/main/java/com/example/rclack/level/current/SynList.java
@@ -0,0 +1,15 @@
+package com.example.rclack.level.current;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//Collections.synchronizedList(new ArrayList<>()); 保存线程不安全的ArrayList
+public class SynList {
+
+    public static void main(String[] args) {
+        List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+        list.add(5);
+        System.out.println(list.get(0));
+    }
+}
